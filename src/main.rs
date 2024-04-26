6fc7bc3b614eec067b5d7ce496bb641d9a4ffe0e
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-04-26 19:36:58 +0530

    Horrible Horrible code for performance, but workd

--- src/main.rs
+++ src/main.rs
@@ -1,235 +1,241 @@
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
-use std::collections::HashMap;
+use image::{GenericImage, Pixel, Rgb};
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-struct Vec2([i8; 2]);
+struct Grid {
+    data: Vec<Vec<Seat>>,
+    num_possibilities: usize,
+    w: usize,
+    h: usize,
+}
+#[derive(Clone, Debug)]
+struct Seat(Vec<usize>);
 
-impl Vec2 {
-    fn new(x: i8, y: i8) -> Vec2 {
-        Vec2([x, y])
-    }
-    fn flip(&self) -> Vec2 {
-        Vec2([-self.0[0], -self.0[1]])
-    }
+#[derive(Clone, PartialEq, Debug)]
+enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
 }
 
-#[derive(PartialEq, Clone)]
-struct Rule {
-    // The 'WEIGHTAGE' / how likely is it that this cell will spawn
-    weight: f32,
-    // The cell the rules is being defined for
-    cell: CellType,
-    // A hashmap of cells that are allowed to be around this cell
-    // to the Vec of the directions it can be
-    cell_map: HashMap<Vec2, Vec<CellType>>,
+fn shannon_entropy(seat: &Seat, weights: &Vec<f32>) -> f32 {
+    let sum: f32 = seat.0.iter().map(|v| weights[*v]).sum();
+    let log_sum: f32 = seat
+        .0
+        .iter()
+        .map(|v| weights[*v] * weights[*v].log2())
+        .sum();
+    sum.log2() - log_sum / sum
 }
 
-#[derive(PartialEq, Clone)]
-struct Rules(Vec<Rule>);
+// (SEA, COAST, LEFT) means that SEA can exist to the LEFT of COAST
+#[derive(Clone, PartialEq, Debug)]
+struct Rule(usize, usize, Direction);
 
-impl Rules {
-    fn get_rule(&self, cell: CellType) -> Option<&Rule> {
-        return self.0.iter().filter(|r| r.cell == cell).next();
+#[derive(Clone)]
+enum GridState {
+    Uncollapsed,
+    Collapsed,
+    Contradicting,
+}
+
+impl Grid {
+    fn new(w: usize, h: usize, num_possibilities: usize) -> Grid {
+        Grid {
+            data: vec![vec![Seat::new(num_possibilities); w]; h],
+            num_possibilities,
+            w,
+            h,
+        }
     }
-    fn get_possibilities(&self, cell: CellType, direction: &Vec2) -> Vec<CellType> {
-        self.0
+    fn get_state(&self) -> GridState {
+        if self
+            .data
             .iter()
-            .filter(|r| r.cell == cell)
-            .next()
-            .unwrap()
-            .cell_map
+            .map(|v| v.iter())
+            .flatten()
+            .any(|p| p.0.len() == 0)
+        {
+            GridState::Contradicting
+        } else if self
+            .data
             .iter()
-            .filter(|(dir, _)| *dir == direction)
-            .map(|(_, cells)| (*cells).clone())
-            .collect::<Vec<Vec<CellType>>>()
-            .concat()
-    }
-}
-
-impl Rule {
-    fn new(cell: CellType, weight: f32) -> Rule {
-        return Rule {
-            weight,
-            cell,
-            cell_map: HashMap::new(),
-        };
-    }
-    fn push_cell_set(&mut self, cell: CellType, direction: Vec2) {
-        if self.cell_map.contains_key(&direction) {
-            self.cell_map.get_mut(&direction).unwrap().push(cell);
+            .map(|v| v.iter())
+            .flatten()
+            .all(|p| p.0.len() == 1)
+        {
+            GridState::Collapsed
         } else {
-            self.cell_map.insert(direction, vec![cell]);
+            GridState::Uncollapsed
         }
     }
-}
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-enum CellType {
-    // When the cell hasn't collapsed, it stores the types it can be
-    Uncertain(Vec<CellType>),
-    // When the cell has collapsed, it stores the type it was
-    Space,
-    //Gas,
-    //Plasma,
-    Core,
-    // When Cell is initialized, it is of type None
-    // It represents the state when the cell could be of any type
-    None,
-}
-
-impl CellType {
-    fn get_color(&self) -> image::Rgb<u8> {
-        match self {
-            CellType::None => image::Rgb::from([0, 0, 0]),
-            CellType::Core => image::Rgb::from([230, 220, 200]),
-            //CellType::Plasma => image::Rgb::from([240, 240, 240]),
-            //CellType::Gas => image::Rgb::from([240, 240, 240]),
-            CellType::Space => image::Rgb::from([20, 20, 50]),
-            CellType::Uncertain(_) => image::Rgb::from([0, 0, 0]),
+    fn possibilities(
+        &self,
+        surrounding: &Seat,
+        direction: &Direction,
+        rules: &Vec<Rule>,
+    ) -> Vec<usize> {
+        let mut temp_possibilities = Vec::new();
+        for cell_type in surrounding.0.iter() {
+            for v in rules
+                .iter()
+                .filter(|v| v.1 == *cell_type && v.2 == *direction)
+                .map(|v| v.0)
+            {
+                if !temp_possibilities.contains(&v) {
+                    temp_possibilities.push(v)
+                }
+            }
         }
+        temp_possibilities
     }
-}
-
-#[derive(PartialEq, Clone)]
-struct Field {
-    width: u32,
-    height: u32,
-    cells: Vec<CellType>,
-    rules: Rules,
-}
-
-impl Field {
-    fn new(w: u32, h: u32, rules: Rules) -> Field {
-        return Field {
-            width: w,
-            height: h,
-            cells: vec![CellType::None; (w * h) as usize],
-            rules,
-        };
+    fn step(&mut self, rules: &Vec<Rule>, weights: &Vec<f32>) {
+        let mut min_entropy = (0, 0, std::f32::MAX);
+        for y in 0..self.h {
+            for x in 0..self.w {
+                if self.data[y][x].0.len() == 1 {
+                    continue;
+                }
+                let current_entropy = shannon_entropy(&self.data[y][x], weights);
+                if current_entropy < min_entropy.2 {
+                    min_entropy = (y, x, current_entropy);
+                }
+            }
+        }
+        let mut possibilties_collection = Vec::new();
+        let (y, x, _) = min_entropy;
+        if y != 0 {
+            let up_possibilities = self.possibilities(&self.data[y - 1][x], &Direction::UP, &rules);
+            possibilties_collection.push(up_possibilities);
+        }
+        if y < self.h - 1 {
+            let down_possibilities =
+                self.possibilities(&self.data[y + 1][x], &Direction::DOWN, &rules);
+            possibilties_collection.push(down_possibilities);
+        }
+        if x < self.w - 1 {
+            let right_possibilities =
+                self.possibilities(&self.data[y][x + 1], &Direction::RIGHT, &rules);
+            possibilties_collection.push(right_possibilities);
+        }
+        if x != 0 {
+            let left_possibilities =
+                self.possibilities(&self.data[y][x - 1], &Direction::LEFT, &rules);
+            possibilties_collection.push(left_possibilities);
+        }
+        let mut possibilities = possibilties_collection.pop().unwrap();
+        for p in possibilties_collection.iter() {
+            possibilities = p
+                .iter()
+                .filter(|v| possibilities.contains(v))
+                .map(|v| *v)
+                .collect();
+        }
+        possibilities.sort_by(|v0, v1| weights[*v0].total_cmp(&weights[*v1]));
+        let mut sampled_idx = None;
+        let some_random_number: f32 = rand::random();
+        for possibility in possibilities.iter() {
+            if some_random_number < weights[*possibility] {
+                sampled_idx = Some(possibility);
+                break;
+            }
+        }
+        if sampled_idx.is_none() {
+            sampled_idx = possibilities.last();
+        }
+        self.data[min_entropy.0][min_entropy.1] = Seat(vec![*sampled_idx.expect("Something should have been sampled")]);
     }
-    fn get_color(&self, x: u32, y: u32) -> image::Rgb<u8> {
-        self.cells[(self.width * y + x) as usize].get_color()
-    }
-    fn set(&mut self, x: u32, y: u32, tile: &CellType) {
-        self.cells[(y * self.width + x) as usize] = tile.clone();
-    }
-    fn set_dir(&mut self, cx: u32, cy: u32, dir: &Vec2, tile: &CellType) {
-        self.set(
-            (cx as i32 + dir.0[0] as i32) as u32,
-            (cy as i32 + dir.0[1] as i32) as u32,
-            tile,
-        );
-    }
-    fn observe(&mut self, x: u32, y: u32) {
-        let tile = self.get(x, y);
-        let binding = self.clone();
-        println!("{:?}", tile);
-        let rule = binding
-            .rules
-            .get_rule(tile)
-            .expect("There is no rule matching this tile");
-        for (direction, cells) in rule.cell_map.iter() {
-            let mut uncertain_cell = CellType::Uncertain(vec![]);
-            for cell in cells.iter() {
-                println!("x: {}, y: {}, direction: {:?}", x, y, direction);
-                match self.get_dir(x, y, direction) {
-                    CellType::Uncertain(v) => {
-                        if v.len() == 1 {
-                            self.set_dir(x, y, direction, &v[0]);
-                            return;
-                        }
-                        uncertain_cell = match uncertain_cell {
-                            CellType::Uncertain(possibilities) => CellType::Uncertain(
-                                possibilities
-                                    .iter()
-                                    .chain(&mut v.iter().filter(|c| *c == cell))
-                                    .map(|c| c.clone())
-                                    .collect(),
-                            ),
-                            _ => CellType::None,
-                        };
-                        self.set_dir(x, y, direction, &uncertain_cell);
-                    }
-                    CellType::None => {
-                        for rule in binding.rules.0.iter() {
-                            for direction in rule.cell_map.keys() {
-                                match self.get_dir(x, y, direction) {
-                                    CellType::None => {}
-                                    x => {
-                                        let possibilites =
-                                            self.rules.get_possibilities(x, direction);
-                                        uncertain_cell = match uncertain_cell {
-                                            CellType::Uncertain(possibilities_union) => {
-                                                CellType::Uncertain(
-                                                    possibilities_union
-                                                        .iter()
-                                                        .chain(
-                                                            &mut possibilites
-                                                                .iter()
-                                                                .filter(|c| *c == cell),
-                                                        )
-                                                        .map(|c| c.clone())
-                                                        .collect(),
-                                                )
-                                            }
-                                            _ => CellType::None,
-                                        };
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    _ => self.observe(
-                        (x as i32 + direction.0[0] as i32) as u32,
-                        (y as i32 + direction.0[1] as i32) as u32,
-                    ),
-                };
+    fn collapse(&mut self, rules: &Vec<Rule>, weights: &Vec<f32>) {
+        match self.get_state() {
+            GridState::Uncollapsed => {
+                self.step(rules, weights);
+                self.collapse(rules, weights)
+            }
+            GridState::Collapsed => {}
+            GridState::Contradicting => {
+                self.data = vec![vec![Seat::new(self.num_possibilities); self.w]; self.h];
+                self.collapse(rules, weights);
             }
         }
     }
-    fn render(&self, _dest: String) {
-        let mut img = image::RgbImage::new(self.width, self.height);
-        for x in 0..self.width {
-            for y in 0..self.height {
-                img.put_pixel(x, y, self.get_color(x, y))
+}
+
+impl Seat {
+    fn new(num_possibilities: usize) -> Seat {
+        Seat(Vec::from_iter(0..num_possibilities))
+    }
+}
+
+fn generate_rules<T: GenericImage<Pixel = P>, P: Pixel + std::cmp::PartialEq>(
+    img: T,
+) -> (Vec<Rule>, Vec<f32>, Vec<P>) {
+    let mut rules = Vec::new();
+    let mut colors = Vec::new();
+    let mut weights: Vec<usize> = Vec::new();
+    for x in 0..(img.width()) {
+        for y in 0..(img.height()) {
+            let a = img.get_pixel(x, y);
+            match colors.iter().position(|v| *v == a) {
+                None => {
+                    colors.push(a);
+                    weights.push(1);
+                }
+                Some(i) => {
+                    weights[i] += 1;
+                }
+            };
+            if !colors.contains(&a) {
+                colors.push(a);
             }
         }
     }
-    fn to_u32_arr(&self) -> Vec<u32> {
-        self.cells
-            .iter()
-            .map(|cell| {
-                let color = cell.get_color();
-                (color.0[2]) as u32 | (((color.0[1]) as u32) << 8) | ((color.0[0]) as u32) << 16
-            })
-            .collect()
+    let weights = weights
+        .into_iter()
+        .map(|v| v as f32 / colors.len() as f32)
+        .collect();
+    for x in 1..(img.width() - 1) {
+        for y in 1..(img.height() - 1) {
+            let current = img.get_pixel(x, y);
+            let current_id = colors.iter().position(|v| *v == current).unwrap();
+            let left = img.get_pixel(x - 1, y);
+            let left_id = colors.iter().position(|v| *v == left).unwrap();
+            let right = img.get_pixel(x + 1, y);
+            let right_id = colors.iter().position(|v| *v == right).unwrap();
+            let up = img.get_pixel(x, y + 1);
+            let up_id = colors.iter().position(|v| *v == up).unwrap();
+            let down = img.get_pixel(x, y - 1);
+            let down_id = colors.iter().position(|v| *v == down).unwrap();
+
+            if !rules.contains(&Rule(left_id, current_id, Direction::LEFT)) {
+                rules.push(Rule(left_id, current_id, Direction::LEFT))
+            }
+            if !rules.contains(&Rule(right_id, current_id, Direction::RIGHT)) {
+                rules.push(Rule(right_id, current_id, Direction::RIGHT))
+            }
+            if !rules.contains(&Rule(up_id, current_id, Direction::UP)) {
+                rules.push(Rule(up_id, current_id, Direction::UP))
+            }
+            if !rules.contains(&Rule(down_id, current_id, Direction::DOWN)) {
+                rules.push(Rule(down_id, current_id, Direction::DOWN))
+            }
+        }
     }
-    fn get(&self, x: u32, y: u32) -> CellType {
-        self.cells[(self.width * y + x) as usize].clone()
-    }
-    fn get_dir(&mut self, cx: u32, cy: u32, dir: &Vec2) -> CellType {
-        self.get(
-            (cx as i32 + dir.0[0] as i32) as u32,
-            (cy as i32 + dir.0[1] as i32) as u32,
-        )
-    }
+    (rules, weights, colors)
 }
 
 fn main() {
-    let mut window = Window::new("Wave Function Collapse", 80, 80, WindowOptions::default())
-        .expect("Should have opened a window");
-    let mut rules = vec![];
-    let mut rule = Rule::new(CellType::Core, 0.5);
-    rule.push_cell_set(CellType::Space, Vec2::new(1, 1));
-    rules.push(rule);
-    let mut rule = Rule::new(CellType::Space, 0.5);
-    rule.push_cell_set(CellType::Core, Vec2::new(-1, -1));
-    rules.push(rule);
-    let mut f = Field::new(80, 80, Rules(rules));
-    f.set(50, 50, &CellType::Core);
-    f.observe(50, 50);
-    window.limit_update_rate(Some(std::time::Duration::from_millis(4)));
-    let buf = f.to_u32_arr();
-    while window.is_open() || !window.is_key_pressed(Key::Escape, KeyRepeat::Yes) {
-        window.update_with_buffer(&buf, 80, 80).unwrap();
-    }
+    let (rules, weights, colors) = generate_rules(image::open("test.png").unwrap());
+    let mut grid = Grid::new(200, 200, colors.len());
+    grid.data[0][0] = Seat(vec![0]);
+    grid.collapse(&rules, &weights);
+    let buffer = grid
+        .data
+        .iter()
+        .map(|v| v.iter())
+        .flatten()
+        .map(|v| v.0.first().unwrap())
+        .map(|v| colors[*v].0)
+        .flatten()
+        .collect();
+    let a = image::RgbaImage::from_vec(grid.w as u32, grid.h as u32, buffer).unwrap();
+    a.save("temp_out.png").unwrap();
 }
